765f46177f0c23c1a82f86de5f9b5dee1dfdbf3f
crashzk <crashzk@zkservidores.com>
2026-03-28 08:26:24 -0300

    Create accept-cookie.php

---
+++ Resources/lang/br/accept-cookie.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Usamos cookies',
+    'description' => 'Usamos cookies para garantir a estabilidade do site. Ao continuar utilizando o site, você concorda com nossa política de cookies.',
+    'accept' => 'Aceitar tudo',
+    'settings' => 'Configurações',
+    'privacy_policy' => 'Política de Privacidade',
+    'learn_more' => 'Saiba mais',
+];
